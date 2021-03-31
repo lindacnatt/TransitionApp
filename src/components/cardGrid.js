a6279b38545ae900909d92a955f7c4fab6c9d084
Linda <lindacn@kth.se>
2021-03-31 12:40:38 +0200

    Clean up code
    new cardgrid component

---
+++ src/components/cardGrid.js
@@ -0,0 +1,33 @@
+import * as React from "react";
+import { useState } from "react";
+import Layout from "../components/layout";
+import Grid from "@material-ui/core/Grid";
+import Card1 from "../components/card.js";
+import CardDetail from "../components/cardDetail.js";
+import "../pages/style.css";
+
+const CardGrid = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [easingClass, setEasingClass] = useState("");
+  const handleClickOnCard = (easingType) => {
+    setEasingClass(easingType);
+    setIsOpen(!isOpen);
+  };
+  return (
+    <Grid container spacing={2} justify="center" alignItems="center">
+      {props.order.map((item) => (
+        <Grid onClick={() => handleClickOnCard(item.easing)} item xs={6}>
+          <Card1
+            title="Icon"
+            imageSource={require("../images/" + item.image)}
+          />
+        </Grid>
+      ))}
+      <div className={`menu ${isOpen ? "active" : "inactive"} ${easingClass}`}>
+        <CardDetail />
+      </div>
+    </Grid>
+  );
+};
+
+export default CardGrid;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,18 @@
 import * as React from "react";
 import { useState } from "react";
 import Layout from "../components/layout";
-import Grid from "@material-ui/core/Grid";
-import Card1 from "../components/card.js";
-import CardDetail from "../components/cardDetail.js";
+import CardGrid from "../components/cardGrid.js"
 import "./style.css";
 
 const IndexPage = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [easingClass, setEasingClass] = useState("");
+/*   const [easingClass, setEasingClass] = useState("");
   const handleClickOnCard = (easingType) => {
     setEasingClass(easingType);
     setIsOpen(!isOpen);
-   };
+   }; */
 
-  const random = [
+  const random1 = [
     {
       image: "flower.jpeg",
       easing: "easing1",
@@ -32,21 +30,27 @@ const IndexPage = () => {
       easing: "easing4",
     },
   ];
+  const random2 = [
+    {
+      image: "icon.png",
+      easing: "easing2",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing3",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing1",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing4",
+    },
+  ];
   return (
     <Layout>
-      <Grid container spacing={2} justify="center" alignItems="center">
-        {random.map((item) => (
-          <Grid onClick={ () => handleClickOnCard(item.easing) } item xs={6}>
-            <Card1
-              title="Icon"
-              imageSource={require("../images/" + item.image)}
-            />
-          </Grid>
-        ))}
-      </Grid>
-      <div className={`menu ${isOpen ? "active" : "inactive"} ${easingClass}`}>
-        <CardDetail />
-      </div>
+      <CardGrid order={random1}/>
     </Layout>
   );
 };
