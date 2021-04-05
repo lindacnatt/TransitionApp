596b8b5e3a2e03955c44fb5bf1c1fffa70e2288b
Linda <lindacn@kth.se>
2021-04-05 13:12:22 +0200

    add flip shell

---
+++ src/pages/flip.js
@@ -0,0 +1,60 @@
+import * as React from "react";
+import Layout from "../components/layout";
+import CardGrid from "../components/cardGrid.js"
+import "./style.css";
+import Footer from "../components/footer";
+
+const FlipPage = () => {
+  const random1 = [
+    {
+      image: "flower.jpeg",
+      easing: "easing1",
+    },
+    {
+      image: "icon.png",
+      easing: "easing2",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing3",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing4",
+    },
+  ];
+  const random2 = [
+    {
+      image: "icon.png",
+      easing: "easing2",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing3",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing1",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing4",
+    },
+    {
+      image: "flower.jpeg",
+      easing: "easing1",
+    },
+    {
+      image: "icon.png",
+      easing: "easing4",
+    },
+  ];
+  return (
+    <Layout>
+      <CardGrid order={random2} animationStyle={"expand"}/>
+      <Footer round={"4"} nextPage={"/"}/>
+    </Layout>
+  );
+};
+
+export default FlipPage;
